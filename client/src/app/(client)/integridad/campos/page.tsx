cdb4e4751667c5a9e2359809839bc85234842413
brunomont <brunoadrian.mont@gmail.com>
2024-03-24 12:15:58 -0500

    Add empty pages for Integridad Campos, Integridad Tablas, Personalizadas, and Secuencialidad 🤡

---
+++ client/src/app/(client)/integridad/campos/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Integridad Campos - Database Auditor',
+}
+
+export default function Page() {
+    return <div>Integridad Campos</div>
+}
