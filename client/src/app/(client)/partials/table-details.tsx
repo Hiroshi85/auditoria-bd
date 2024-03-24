1f8435ede00c3d7c1792adcd286a66d81a36ab15
brunomont <brunoadrian.mont@gmail.com>
2024-03-23 22:35:11 -0500

    Add ExceptionOptions component and update TableDetails layout

--- client/src/app/(client)/partials/table-details.tsx
+++ client/src/app/(client)/partials/table-details.tsx
@@ -3,6 +3,7 @@ import { DataTable } from "@/components/layout/table/table-details";
 import { useTablesDB } from "./tables.context";
 import { columns } from "./columns";
 import { Column } from "@/types/database";
+import ExceptionOptions from "./exception-options";
 
 export default function TableDetails() {
     const data = useTablesDB()
@@ -18,12 +19,15 @@ export default function TableDetails() {
     return (
         <div className="w-full h-full px-5">
             <div>
-                <h3 className="font-bold text-4xl">Tabla: {data.currentTable}</h3>
+                <h3 className="font-bold text-4xl mb-5">Tabla: {data.currentTable}</h3>
                 {
-                    data.query.isLoading ?  (
+                    data.query.isLoading ? (
                         <p>Cargando...</p>
                     ) : (
-                        <DataTable columns={columns} data={data.query.data?.columns as Column[]}/>
+                        <div className="flex flex-col gap-5">
+                            <ExceptionOptions />
+                            <DataTable columns={columns} data={data.query.data?.columns as Column[]} />
+                        </div>
                     )
                 }
             </div>
