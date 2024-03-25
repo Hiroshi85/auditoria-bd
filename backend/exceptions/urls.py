8d455c6ffd86f821a4ccab83c77c3a48ce36c5ac
Hiroshi <ebruno.hiroshi@gmail.com>
2024-03-24 23:12:51 -0500

    add: initial campos_exception_view

--- backend/exceptions/urls.py
+++ backend/exceptions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import main, campos_exception_view
 
 urlpatterns = [
-    path('sequence/', view=sequence_exception),
+    path('campos/conexion/<int:id>', view=campos_exception_view.obtain_valores),
+    path('sequence/', view=main.sequence_exception),
   
 ]
