ab88381e095019755ba364b43f4759a37b3acf58
JohannCastillo <johannjco15022@gmail.com>
2024-03-25 19:14:21 -0500

    add: excepciones secuenciales para registros alfanuméricos por ej PROD00001 PROD00002 ....

--- backend/exceptions/serializers.py
+++ backend/exceptions/serializers.py
@@ -4,9 +4,18 @@ from rest_framework.fields import ChoiceField
 class SequentialSerializer (serializers.Serializer):
     table = serializers.CharField(required=True)
     column = serializers.CharField(required=True)
-    example = serializers.CharField(required=False)  # alfanuméricos
     min = serializers.CharField(required=False)
     max = serializers.CharField(required=False)
+    step = serializers.IntegerField(required=False)
     sort = serializers.CharField(required=False)
-    # D, M o Y para secuencia de fechas de días, meses o años
-    frequency = ChoiceField(choices=['D', 'M', 'Y'], required=False)
+
+    # alfanuméricos
+    example = serializers.CharField(required=False) 
+    static = serializers.BooleanField(required=False) # letras estáticas en la secuencia
+
+    # fechas 
+    # D (días)  
+    # ME (fin de mes), MS(inicio de mes) 
+    # YE(fin de año), YS(inicio de año)
+    frequency_options = ['D', 'ME', 'MS', 'YE', 'YS'] 
+    frequency = ChoiceField(choices=frequency_options, required=False)
