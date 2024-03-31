dc96339d5c17db120278e01a8e37e502d86c0153
JohannCastillo <johannjco15022@gmail.com>
2024-03-31 18:59:08 -0500

    add: componente de resultados

--- client/src/app/(client)/personalizados/partials/results.tsx
+++ client/src/app/(client)/personalizados/partials/results.tsx
@@ -2,37 +2,15 @@
 import Spinner from "@/components/ui/spinner";
 import { usePersonalizadas } from "../personalizados.context";
 import { cn } from "@/lib/utils";
+import { ResultContainer } from "@/components/ui/result-container";
 import { DataTable } from "@/components/layout/table/table-details";
 import { useConnectionDatabase } from "@/providers/connection";
 import { Table2Icon } from "lucide-react";
-interface ResultsProps {
-  children: React.ReactNode;
-  className?: string;
-  type?: "error" | "ok";
-}
 
 export default function CustomExceptionResults() {
   const { query } = usePersonalizadas();
   const { data, isLoading, isError } = query;
   const connection = useConnectionDatabase();
-
-  function ResultContainer({ children, className, type }: ResultsProps) {
-    return (
-      <div
-        className={cn(
-          "flex flex-col bg-accent w-full rounded-lg p-4 border",
-          type === "ok"
-            ? "border-green-500 bg-white"
-            : type === "error"
-            ? "border-red-500 bg-red-200"
-            : "",
-          className
-        )}
-      >
-        {children}
-      </div>
-    );
-  }
 
   if (isLoading)
     return (
---
+++ client/src/components/ui/result-container.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+interface ResultsProps {
+  children: React.ReactNode;
+  className?: string;
+  type?: "error" | "ok";
+}
+export function ResultContainer({
+  children,
+  className,
+  type,
+}: ResultsProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col bg-accent w-full rounded-lg p-4 border",
+        type === "ok"
+          ? "border-green-500 bg-white"
+          : type === "error"
+          ? "border-red-500 bg-red-200"
+          : "",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
