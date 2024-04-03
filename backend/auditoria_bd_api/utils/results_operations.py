20ebd9996f3dfa8c6687825ec21a7fa677de233c
Hiroshi <ebruno.hiroshi@gmail.com>
2024-04-03 18:22:22 -0500

    fix: try except when results cannot be saved

--- backend/auditoria_bd_api/utils/results_operations.py
+++ backend/auditoria_bd_api/utils/results_operations.py
@@ -3,13 +3,17 @@ from ..models import Result
 
 def save_results(response_dict_str, connection_id, exception_type_enum, table, exception_ocurred):
     exception_type_value = ExceptionType.objects.get(id=exception_type_enum.value)
-    result = Result(
-        table=table,
-        results= response_dict_str,
-        connection=connection_id,
-        exception_type=exception_type_value,
-        exception_ocurred=exception_ocurred
-    )
-    result.save()
+    try:
+        result = Result(
+            table=table,
+            results= response_dict_str,
+            connection=connection_id,
+            exception_type=exception_type_value,
+            exception_ocurred=exception_ocurred
+        )
+        result.save()
+    except Exception as e:
+        print(f"Error al guardar resultado\nDetalle: {e}")
+    
     return result
     
