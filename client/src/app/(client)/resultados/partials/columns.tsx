c9bfcbc2f47f0fe736fd2dd8bd354024f163cbfb
brunomont <brunoadrian.mont@gmail.com>
2024-04-12 21:43:51 -0500

    Add moment.js library

--- client/src/app/(client)/resultados/partials/columns.tsx
+++ client/src/app/(client)/resultados/partials/columns.tsx
@@ -5,6 +5,7 @@ import { Resultado } from "@/types/resultados"
 import { ColumnDef } from "@tanstack/react-table"
 import { EyeIcon, Trash2Icon } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import moment from 'moment'
 
 
 export const columns: ColumnDef<Resultado>[] = [
@@ -78,15 +79,8 @@ export const columns: ColumnDef<Resultado>[] = [
             )
         },
         cell: ({ row }) => {
-            const date = new Date(row.getValue("created_at") as string)
-            // format date to DD-MM-YYYY HH:MM:SS
-            const day = date.getDate().toString().padStart(2, '0')
-            const month = (date.getMonth() + 1).toString().padStart(2, '0')
-            const year = date.getFullYear()
-            const hours = date.getHours().toString().padStart(2, '0')
-            const minutes = date.getMinutes().toString().padStart(2, '0')
-            return `${day}-${month}-${year} ${hours}:${minutes}`
-
+            const date = moment(row.getValue("created_at") as string).format('lll')
+            return date
         }
     },
     {
